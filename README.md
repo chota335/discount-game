6001c098981bfe470efb99ade07f3c29398cc22c
chl82412 <chl82412@gmail.com>
2026-04-05 10:07:02 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# discount-game
+
+A web application that shows game deals from various stores.
+
+## Features
+
+* Fetches and displays game deals from the CheapShark API.
+* Uses Firebase Hosting for deployment.
+* Implements a serverless function using Cloud Functions for Firebase to fetch data from the API.
+
+## Deployment
+
+This project is deployed using Firebase Hosting. The live version can be accessed at [https://your-firebase-project-id.web.app](https://your-firebase-project-id.web.app).
--- functions/deals.js
+++ functions/deals.js
@@ -1,13 +1,10 @@
-export async function onRequest(context) {
-  const url = "https://www.cheapshark.com/api/1.0/deals?storeID=1&pageSize=100";
+const functions = require("firebase-functions");
+const fetch = require("node-fetch");
 
+exports.deals = functions.https.onRequest(async (request, response) => {
+  const url = "https://www.cheapshark.com/api/1.0/deals?storeID=1&pageSize=100";
   const res = await fetch(url);
   const data = await res.json();
-
-  return new Response(JSON.stringify(data), {
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*"
-    }
-  });
-}
+  response.set("Access-Control-Allow-Origin", "*");
+  response.json(data);
+});
